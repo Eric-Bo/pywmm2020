x_prime, y_prime and z_prime implemented but y_prime and z_prime give wrong results

pywmm2020.py
@@ -1,5 +1,6 @@
 from scipy.special import lpmn
 import numpy as np
+import math
 from loader import load_coeff
 
 """
@@ -40,7 +41,7 @@ def transform_to_spherical(lamb, phi, h):
     A = 6378137
     one_over_f = 298.257223563
     f = 1/one_over_f
-    e_squared = (f) * (2 - f)
+    e_squared = f * (2 - f)
     R_c = A / np.sqrt(1 - e_squared * np.sin(phi)**2)
 
     # equations
@@ -76,7 +77,7 @@ def s_legendre(m, n, phi):
     else:
         normalization = np.sqrt(2 * np.math.factorial(n - m) / 
                                 np.math.factorial(n + m))
-        return normalization * lpmn(m, n, phi)[0][-1,-1]
+        return ((-1)**m) * normalization * lpmn(m, n, phi)[0][-1,-1]
 
 
 def pnm_der(m, n, phi):
@@ -86,7 +87,7 @@ def pnm_der(m, n, phi):
     """
     sum1 = (n + 1) * np.tan(phi) * s_legendre(m, n, np.sin(phi))
     sum2 = np.sqrt((n+1)**2 - m**2) * (1/np.cos(phi)) * \
-                s_legendre(m, n, np.sin(phi))
+                s_legendre(m, n+1, np.sin(phi))
     return sum1 - sum2
 
 
@@ -99,7 +100,31 @@ def x_prime(lamb, phi_prime, r, t):
             prod2 = pnm_der(m, n, phi_prime)
             inner_result += prod1 * prod2
         result += inner_result * (a / r)**(n + 2)
-    return result
+    return -1 * result
+
+
+def y_prime(lamb, phi_prime, r, t):
+    result = 0
+    for n in range(1, N+1):
+        inner_result = 0
+        for m in range(n+1):
+            prod1 = g(n, m, t) * np.sin(m * lamb) - h(n, m, t) * np.cos(m*lamb)
+            prod2 = m * s_legendre(m, n, phi_prime)
+            inner_result += prod1 * prod2
+        result += inner_result * (a / r)**(n + 2)
+    return (1/np.cos(phi_prime)) * result
+
+
+def z_prime(lamb, phi_prime, r, t):
+    result = 0
+    for n in range(1, N+1):
+        inner_result = 0
+        for m in range(n+1):
+            prod1 = g(n, m, t) * np.cos(m * lamb) + h(n, m, t) * np.sin(m*lamb)
+            prod2 = s_legendre(m, n, phi_prime)
+            inner_result += prod1 * prod2
+        result += inner_result * ((a / r)**(n + 2)) * (n + 1)
+    return -1 * result
 
 
 if __name__ == "__main__":
@@ -110,7 +135,7 @@ if __name__ == "__main__":
     glon = 240
     glat = glat * (np.pi/180)
     glon = glon * (np.pi/180)
-    #print(transform_to_spherical(glon, glat, alt_km))
+    print(transform_to_spherical(glon, glat, alt_km))
     lamb, phi_prime, r = transform_to_spherical(glon, glat, alt_km)
     """
     print(g(1,0,t))
@@ -126,3 +151,5 @@ if __name__ == "__main__":
     """
     #print(s_legendre(1,1,phi_prime))
     print(x_prime(lamb, phi_prime, r, t))
+    print(y_prime(lamb, phi_prime, r, t))
+    print(z_prime(lamb, phi_prime, r, t))